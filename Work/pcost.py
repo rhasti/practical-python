4efb2418c61c5385f7b7b6c6a79ce1be83947297
Johannes Hofmann <johannes.hofmann@global.ntt>
2021-04-14 18:05:23 +0200

    ex 1.27

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,14 @@
 # pcost.py
 #
 # Exercise 1.27
+sum = 0
+f = open('Data/portfolio.csv', 'rt')
+headers = next(f).split(',')
+for line in f:
+    row = line.split(',')
+    sum += int(row[1]) * float(row[2])
+
+print("Total cost", sum)
+
+
+f.close()
