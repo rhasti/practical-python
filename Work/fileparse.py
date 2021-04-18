10f038624aabb4297acb79f8b2376134a0e0ac7a
Johannes Hofmann <rhasti@gmail.com>
2021-04-18 11:48:07 +0200

    ex 3.4

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename):
+def parse_csv(filename: str, select: list = None ) -> list:
     '''
     Parse a CSV file into a list of records
     '''
@@ -13,9 +13,20 @@ def parse_csv(filename):
         # Read the file headers
         headers = next(rows)
         records = []
+        
+        if select:
+            indices : list = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
         for row in rows:
             if not row:    # Skip rows with no data
                 continue
+            # Filter the row if specific columns were selected
+            if indices:
+                row = [ row[index] for index in indices ]
+
+            # Make a dictionary
             record = dict(zip(headers, row))
             records.append(record)
 
