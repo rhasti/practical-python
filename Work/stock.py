89e1194042f8c01f16714074830be93eccbc18ab
Johannes Hofmann <rhasti@gmail.com>
2021-11-28 16:42:14 +0100

    ex 5.7, 5.8

--- Work/stock.py
+++ Work/stock.py
@@ -1,7 +1,12 @@
+from sys import settrace
+
+
 class Stock:
+    __slots__ = ("name", "_shares", "price")
+
     def __init__(self, name: str, shares: int, price: float) -> None:
         self.name = name
-        self.shares = shares
+        self._shares = shares
         self.price = price
 
     def __repr__(self) -> str:
@@ -10,6 +15,16 @@ class Stock:
     @property
     def cost(self):
         return self.shares * self.price
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, shares):
+        if not isinstance(shares, int):
+            raise TypeError("expected an integer")
+        self._shares = shares
 
     def sell(self, amount):
         if amount <= self.shares:
